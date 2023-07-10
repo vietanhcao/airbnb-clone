73d322771b2187337022b5bc7e35f883f619cd3f
vietanhcao <vietanhcao1994@gmail.com>
2023-07-11 01:14:42 +0700

    feat(navbar): added Navbar UI

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+	const router = useRouter();
+	return (
+		<Image
+			src="/images/logo.png"
+			alt="Logo"
+			className="
+        hidden
+        md:block
+        cursor-pointer
+      "
+			width="100"
+			height="100"
+		/>
+	);
+};
+
+export default Logo;
