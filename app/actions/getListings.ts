1db91fb5246a2e8a5549490bf6ac26fb6dead4af
vietanhcao <vietanhcao1994@gmail.com>
2023-07-21 07:49:40 +0700

    feat(listing): add fetching listing and ui Listing card

---
+++ app/actions/getListings.ts
@@ -0,0 +1,16 @@
+import prisma from "../libs/prismadb";
+
+export default async function getListings() {
+	try {
+		const listings = await prisma.listing.findMany({
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
+
+		return listings;
+	} catch (error) {
+		console.log("getListings error", error);
+		throw new Error("something went wrong");
+	}
+}
