946de0bc365551e9a88d7fd074045e0ae39cca4e
vietanhcao <vietanhcao1994@gmail.com>
2023-07-29 16:58:48 +0700

    feat(trips): update api cancel reservation

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import getCurrentUser from "../../../actions/getCurrentUser";
+import prisma from "../../../libs/prismadb";
+
+interface IParams {
+	reservationId: string;
+}
+
+export async function DELETE(req: Request, { params }: { params: IParams }) {
+	const currentUser = await getCurrentUser();
+	if (!currentUser) {
+		return NextResponse.error();
+	}
+
+	const { reservationId } = params;
+
+	if (!reservationId || typeof reservationId !== "string") {
+		throw new Error("Invalid reservationId");
+	}
+
+	// delete many??
+	const reservation = await prisma.reservation.deleteMany({
+		where: {
+			id: reservationId,
+			OR: [
+				{
+					userId: currentUser.id,
+				},
+				{ listing: { userId: currentUser.id } },
+			],
+		},
+	});
+
+	return NextResponse.json(reservation);
+}
